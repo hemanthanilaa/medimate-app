1a6a15cb4f0403cb7dfd06589538a2a2399bf455
hemanthanilaa <hemanthanilaa@gmail.com>
2025-06-14 22:50:21 +0530

    added analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,12 @@
-import "../styles/globals.css";
+import { Analytics } from "@vercel/analytics/next"
 
-export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+function App({ Component, pageProps }) {
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
+
+export default App;
